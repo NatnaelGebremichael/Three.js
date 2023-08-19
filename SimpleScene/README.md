1ea2ef0a1f1b19d9f937889448273d3c1c2ab8d3
Natnael Gebremichael <natnael92460@gmail.com>
2023-08-19 20:49:38 +1200

    Created a simple cube using three.js

--- SimpleScene/README.md
+++ SimpleScene/README.md
@@ -5,3 +5,10 @@
 - A JavaScript and HTML files
 
 ## How To Run
+
+- Make sure o download the three.js from https://threejs.org/
+- Extract the downloaded file
+  - open the build folder insde the extracted threejs-master
+  - copy three.min.js
+  - Add it to SimpleScene dir
+-
--- SimpleScene/script.js
+++ SimpleScene/script.js
@@ -1,1 +1,30 @@
-console.log("Hello ThreeJs");
+//Creating Scene
+const scene = new THREE.Scene();
+
+//Create MESH (visable Object), red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh); //adding mesh to the scene
+
+//Camera (for point of view)
+const degress = 75; //fild of view
+const sizes = {
+  //For the Aspect Ratio
+  width: 800,
+  height: 600,
+};
+const camera = new THREE.PerspectiveCamera(degress, sizes.width / sizes.height);
+//move the camera from the center off cube
+camera.position.z = 3;
+camera.position.x = 2; // move in x position
+scene.add(camera);
+
+const canvas = document.querySelector(".webgl"); //getting dom element
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height); //resize the renderer (this will also resize the canvas in (html))
+renderer.render(scene, camera);
