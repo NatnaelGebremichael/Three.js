279f6c6ad27e1dbce665a38af75397e329ca3655
Natnael Gebremichael <natnael92460@gmail.com>
2023-12-31 21:19:39 +1300

    LN-9 Final Project in Geometery

--- 09-Geometries/src/script.js
+++ 09-Geometries/src/script.js
@@ -9,9 +9,18 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
+const geometry = new THREE.BufferGeometry();
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const count = 50;
+const positionArray = new Float32Array(count * 3 * 3);
+
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionArray[i] = Math.random() - 0.5;
+}
+
+const positionAttribute = new THREE.BufferAttribute(positionArray, 3);
+geometry.setAttribute("position", positionAttribute);
+
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
   wireframe: true,
